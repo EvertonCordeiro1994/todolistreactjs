dcceb18ec27d9f03a3818e64c552190e6a20a492
Everton Cordeiro <99029999+EvertonCordeiro1994@users.noreply.github.com>
2023-02-03 16:18:03 -0300

    Add files via upload

---
+++ src/TodoList.jsx
@@ -0,0 +1,98 @@
+import React, {useState, useEffect } from  'react';
+import './Todolist.css';
+import Icon from './assets/icon.webp'
+
+function TodoList(){
+
+    const listaStorage = localStorage.getItem('Lista');
+    const [novoItem, setNovoItem ] = useState('');
+    const [lista, setLista] = useState( listaStorage ? JSON.parse(listaStorage) : []);
+    
+    useEffect(
+        ()=>{
+            localStorage.setItem('Lista', JSON.stringify(lista));
+        },[lista])
+
+    function adicionarItem(form){
+        form.preventDefault();
+        if(!novoItem){
+            return;
+        }else{
+            setLista([...lista, {text: novoItem, isCompleted: false}])
+            setNovoItem("")
+            document.getElementById('input-entrada').focus();
+        }
+    }
+
+    function clicou(index){
+        const listaAuxiliar = [...lista];
+        listaAuxiliar[index].isCompleted = !listaAuxiliar[index].isCompleted;
+        setLista(listaAuxiliar);
+        
+    }
+
+    function deletar(index){
+        const listaAuxiliar = [...lista];
+        listaAuxiliar.splice(index,1);
+        setLista(listaAuxiliar);
+        
+    }
+    function deletarTudo(){
+        setLista([]);
+        
+    }
+    
+    return (
+        <div>
+            <h1>Lista de tarefas</h1>
+        
+            <form onSubmit={adicionarItem}>
+                <input 
+                    type="text" 
+                    value={novoItem}
+                    placeholder='Adicione a sua tarefa' 
+                    onChange ={
+                        (e)=>{
+                             setNovoItem(e.target.value)
+                        }
+                    }
+                />
+
+                <button className='btn_add' type='submit'>
+                    Add
+                </button>
+            </form>
+
+            <div className="listadetarefas">
+                {
+                    lista.length<1
+                    ?
+                    <img src={Icon} />
+                    :
+                    
+                    lista.map(
+                        (tarefa, index) => 
+                        ( 
+                            <div key={index} className={tarefa. isCompleted ? "tarefaConcluida" : "tarefa"}>
+                            
+                                <span onClick={()=>{clicou(index)}}>{tarefa.text}</span>
+                                <button onClick={()=>{deletar(index)}} className='btn_del'> Excluir </button>
+                            </div>
+                        )
+                    )
+                }
+                {
+                    lista.length > 1 &&
+                    <div>
+                        <button onClick={()=>{deletarTudo()}} className='btn_del_all'>
+                            Deletar concluidas
+                        </button>
+                    </div>
+                }
+
+            </div>
+        </div>
+    )
+} 
+
+export default TodoList
